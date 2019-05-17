1a1feea07ac9d90b3dd6038e37bd92b8bd245979
Ben Boyter <ben@boyter.org>
2019-05-17 10:57:22 +1000

    Start to build

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var verbose = false
+
+//go:generate go run scripts/include.go
+func main() {
+	rootCmd := &cobra.Command{
+		Use:     "hashit",
+		Short:   "hashit [FILE or DIRECTORY]",
+		Long:    "Hash It!\nBen Boyter <ben@boyter.org> + Contributors",
+		Version: "0.1.0",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(args)
+		},
+	}
+
+	flags := rootCmd.PersistentFlags()
+
+
+	flags.BoolVarP(
+		&verbose,
+		"verbose",
+		"v",
+		false,
+		"verbose output",
+	)
+
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
