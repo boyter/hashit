a616b076ceb88a582dd0a9ed81578532fe7cc656
Ben Boyter <ben@boyter.org>
2019-05-24 08:13:27 +1000

    Start to add tests

--- test-all.sh
+++ test-all.sh
@@ -6,10 +6,27 @@ gofmt -s -w ./..
 echo "Running unit tests..."
 go test ./... || exit
 
+echo "Building application..."
+go build -ldflags="-s -w" || exit
+
 
 GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m'
+
+a=$(./hashit --no-stream * | md5sum)
+b=$(./hashit * | md5sum)
+if [ "$a" == "$b" ]; then
+    echo -e "${GREEN}PASSED stream output test"
+else
+    echo -e "${RED}======================================================="
+    echo -e "FAILED stream output test"
+    echo -e "================================================="
+    exit
+fi
+
+echo -e "${NC}Cleaning up..."
+rm ./hashit
 
 echo -e "${GREEN}================================================="
 echo -e "ALL TESTS PASSED"
